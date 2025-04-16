95b76b85c97fad2569ab862f4960a8d7db96828a
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-16 18:31:39 -0400

    code item

---
+++ CodingTracker/Model/CodeItem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Model
+{
+    public class CodeItem
+    {
+        public int Id {get; set;}
+        public int Duration { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
--- CodingTracker/obj/Debug/net9.0/CodingTracker.AssemblyInfo.cs
+++ CodingTracker/obj/Debug/net9.0/CodingTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodingTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb26e83eadc34442947c172392defa1eebdb9cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e6e179c2a0891dee7d0b7c13184191f85ccf74")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodingTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodingTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
